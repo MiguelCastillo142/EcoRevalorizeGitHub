d2d0cf708ab0431941bc8baf8bc8474a16965830
Ariel <ariel.quezada06@inacapmail.cl>
2023-12-02 11:35:04 -0300

    El usuario puede cambiar su username

--- gestorUser/views.py
+++ gestorUser/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .models import Usuario
 from gestorProducto.models import *
-from .forms import SignUpForm
+from .forms import SignUpForm , EditarNombreForm
 from django.contrib.auth.models import User
+from django.contrib import messages
+
+
 
 @login_required(login_url='login')
 def editarUsuario(request, id):
@@ -53,3 +56,17 @@ def listaproducto(request):
     producto = Producto.objects.all()
     data = {'producto': producto}
     return render(request, 'vistaproductos.html', data)
+
+
+@login_required
+def editar_nombre(request):
+    if request.method == 'POST':
+        form = EditarNombreForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '¡Tu nombre ha sido actualizado!')
+            return redirect('/interfaz') 
+    else:
+        form = EditarNombreForm(instance=request.user)
+
+    return render(request, 'editar_nombre.html', {'form': form})
